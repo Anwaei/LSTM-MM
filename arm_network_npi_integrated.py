5ee9dcfb364a0f5e95040ece0c7933f6dca940d2
Anwaei <civact@qq.com>
2022-04-07 20:45:38 +0800

    1

--- arm_network_npi_integrated.py
+++ arm_network_npi_integrated.py
@@ -14,40 +14,60 @@ def create_model(units_mlp_x, units_mlp_s, units_lstm, units_mlp_c):
     for k in range(len(units_mlp_x)):
         unit = units_mlp_x[k]
         if k == 0:
-            out_x = layers.TimeDistributed(layers.Dense(unit, activation='tanh'))(input_x)
+            out_x = layers.TimeDistributed(layers.Dense(unit, activation='tanh',
+                                                        name='x_dense' + str(k)),
+                                           name='x_wrapped_dense' + str(k))(input_x)
         else:
-            out_x = layers.TimeDistributed(layers.Dense(unit, activation='tanh'))(out_x)
+            out_x = layers.TimeDistributed(layers.Dense(unit, activation='tanh',
+                                                        name='x_dense' + str(k)),
+                                           name='x_wrapped_dense' + str(k))(out_x)
 
     for k in range(len(units_mlp_s)):
         unit = units_mlp_s[k]
         if k == 0:
-            out_s = layers.TimeDistributed(layers.Dense(unit, activation='tanh'))(input_s)
+            out_s = layers.TimeDistributed(layers.Dense(unit, activation='tanh',
+                                                        name='s_dense' + str(k)),
+                                           name='s_wrapped_dense' + str(k))(input_s)
         else:
-            out_s = layers.TimeDistributed(layers.Dense(unit, activation='tanh'))(out_s)
+            out_s = layers.TimeDistributed(layers.Dense(unit, activation='tanh',
+                                                        name='s_dense' + str(k)),
+                                           name='s_wrapped_dense' + str(k))(out_s)
 
     for k in range(len(units_lstm)):
         unit = units_lstm[k]
         if len(units_lstm) == 1:
-            out_z = layers.LSTM(units=unit, return_sequences=False)(input_z)
+            out_z = layers.LSTM(units=unit, return_sequences=False, name='z_lstm' + str(k))(input_z)
         else:
             if k == 0:
-                out_z = layers.LSTM(units=unit, return_sequences=True)(input_z)
-            elif k < len(units_lstm) - 1:
-                out_z = layers.LSTM(units=unit, return_sequences=True)(out_z)
+                out_z = layers.LSTM(units=unit, return_sequences=True, name='z_lstm' + str(k))(input_z)
             else:
-                out_z = layers.LSTM(units=unit, return_sequences=True)(out_z)
+                out_z = layers.LSTM(units=unit, return_sequences=True, name='z_lstm' + str(k))(out_z)
 
     out_c = layers.concatenate([out_x, out_s, out_z])
 
     for k in range(len(units_mlp_c)):
         unit = units_mlp_c[k]
-        out_c = layers.TimeDistributed(layers.Dense(unit, activation='sigmoid'))(out_c)
+        out_c = layers.TimeDistributed(layers.Dense(unit, activation='sigmoid',
+                                                    name='c_dense' + str(k)),
+                                       name='c_wrapped_dense' + str(k))(out_c)
 
-    out_final = layers.TimeDistributed(layers.Dense(3, activation='softmax'))(out_c)
+    out_final = layers.TimeDistributed(layers.Dense(3, activation='softmax', name='f_dense'),
+                                       name='f_wrapped_dense' )(out_c)
 
     net = keras.Model(inputs=[input_x, input_s, input_z], outputs=out_final)
 
     return net
+
+
+def one_step_model(rnnmodel):
+
+    model_config = rnnmodel.get_config()
+    layers_config = model_config['layers']
+    x_layers = []
+    s_layers = []
+    z_layers = []
+    c_layers = []
+
 
 
 def process_data(data):
